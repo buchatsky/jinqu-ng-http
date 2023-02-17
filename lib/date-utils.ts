449cc749146f8899ecce180f12964d7ccf11bd16
Alex.Buchatsky <buchatski@bigmir.net>
2023-02-17 21:41:14 +0200

    dateToISOLocalString optimized

--- lib/date-utils.ts
+++ lib/date-utils.ts
@@ -1,37 +1,13 @@
 export function dateToISOLocalString(date: Date): string {
-    function padNum(num: number, digits: number): string {
-        let strNum = String(num);
-        //strNum = '0'.repeat(digits - strNum.length) + strNum;
-        while (strNum.length < digits) {
-            strNum = '0' + strNum;
-        }
-        return strNum;
-    }
-
-    const [month, day, year] = [
-        date.getMonth() + 1,
-        date.getDate(),
-        date.getFullYear(),
-    ];
-    const [hours, minutes, seconds, milliseconds] = [
-        date.getHours(),
-        date.getMinutes(),
-        date.getSeconds(),
-        date.getMilliseconds(),
-    ];
-
     let tzOffset = date.getTimezoneOffset();
-    let tzOffsetStr: string;
+    const isoStr = new Date(date.getTime() - tzOffset * 60 * 1000).toISOString()
     if (tzOffset !== 0) {
         const tzSign = tzOffset > 0 ? '-' : '+';
         tzOffset = Math.abs(tzOffset);
         const tzMins = tzOffset % 60;
         const tzHours = (tzOffset - tzMins) / 60;
-        tzOffsetStr = tzSign + padNum(tzHours, 2) + ':' + padNum(tzMins, 2);
+        return isoStr.slice(0, -1) + tzSign + ('0' + tzHours).slice(-2) + ':' + ('0' + tzMins).slice(-2);
     } else {
-        tzOffsetStr = 'Z';
+        return isoStr;
     }
-
-    const dtStr = `${String(year)}-${padNum(month, 2)}-${padNum(day, 2)}T${padNum(hours, 2)}:${padNum(minutes, 2)}:${padNum(seconds, 2)}.${padNum(milliseconds, 3)}`;
-    return dtStr + tzOffsetStr;
 }
